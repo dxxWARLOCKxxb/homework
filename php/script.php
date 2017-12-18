8722f96f9875b11d971496145a8347477a530858
dxxWARLOCKxxb <legion.const@yandex.ru>
2017-12-18 18:06:25 +0300

    ДЗ по PHP, скрипт, ищущий последнюю версию ядра linux, скачивающий нужные пакеты для 64-х разрядной системы и запускающий обновление

---
+++ php/script.php
@@ -0,0 +1,24 @@
+#!/usr/bin/php
+<?php
+$URL = 'http://kernel.ubuntu.com/~kernel-ppa/mainline/';
+$page = @file_get_contents($URL);
+preg_match_all('#href="(v[\d\.]+/)"#', $page, $last_version);
+$last_version = $last_version[1];
+natsort($last_version);
+$last_version = array("name" => $last_version[count($last_version) - 2], "url" => $URL . $last_version[count($last_version) - 2]);
+echo "Найденная последняя версия ядра linux: {$last_version['name']}\n";
+$page = @file_get_contents($last_version['url']);
+preg_match_all('#href="(\w+\-\w+\-[\d\.\-]+[_|g]+.*_a[lmd64]+\.deb)"#', $page, $links);
+$links = $links[1];
+$links = array_unique($links);
+foreach ($links as $link) {
+    echo "Загружаю $link\n";
+    @file_put_contents($link, @file_get_contents($last_version['url'].$link));
+}
+echo "Запускаю обновление:\n" . shell_exec('sudo dpkg -i *.deb');
+foreach ($links as $link) {
+    echo "Удаляю $link\n";
+    unlink($link);
+}
+?>
+
