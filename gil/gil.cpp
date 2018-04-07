d72d87092521556ce1dfb806ddf2101adae6be70
dxxWARLOCKxxb <legion.const@yandex.ru>
2018-04-07 07:08:45 +0300

    Start GIL project
    Add library files gil.cpp and gil.h, add application file main.cpp and add build file CMakeLists.txt

---
+++ gil/gil.cpp
@@ -0,0 +1,5 @@
+//
+// Created by esdeath on 07.04.18.
+//
+
+#include "gil.h"
---
+++ gil/gil.h
@@ -0,0 +1,8 @@
+//
+// Created by esdeath on 07.04.18.
+//
+
+#ifndef GIL_GIL_H
+#define GIL_GIL_H
+
+#endif //GIL_GIL_H
