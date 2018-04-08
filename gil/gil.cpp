343fad5773dfcd8358c7219f2f2500f43e7d254c
dxxWARLOCKxxb <legion.const@yandex.ru>
2018-04-09 00:13:40 +0300

    Create structure of class Bitmap
    Add class Bitmap with 2 constrctors (load from file and create new image), methods Save and destructor;
    Add structure FileHeader, InfoHeader, RGB_32;
    Add namespace BMP.

--- gil/gil.cpp
+++ gil/gil.cpp
@@ -1,5 +1,24 @@
-//
-// Created by esdeath on 07.04.18.
-//
-
+/**
+ * @file
+ * @author Манаев Константин
+ * @brief Файл с реализацией объявленных в gil.h и внутренних классов и функций
+ */
 #include "gil.h"
+
+namespace BMP {
+    Bitmap::Bitmap(const char *fileName) {
+        //@todo Реализовать конструктор Bitmap::Bitmap(const char *fileName)
+    }
+
+    Bitmap::Bitmap(DWORD width, DWORD height, WORD bitCount) {
+        //@todo Реализовать конструктор Bitmap::Bitmap(DWORD width, DWORD height, WORD bitCount)
+    }
+
+    void Bitmap::Save(const char *filename) const {
+        //@todo Реализовать метод void Bitmap::Save(const char *filename)
+    }
+
+    Bitmap::~Bitmap() {
+        //@todo Реализовать деструктор Bitmap::~Bitmap()
+    }
+}
--- gil/gil.h
+++ gil/gil.h
@@ -1,8 +1,85 @@
-//
-// Created by esdeath on 07.04.18.
-//
-
+/**
+ * @headerfile
+ * @author Манаев Константин
+ * @brief Классы и функций необходимые для выполнения лаборатоных работ по дисциплине "Компьютерная обработка изображений"
+ * @warning Для работы рекомендуется использовать только функции классы, размещенные в namespace BMP
+ */
 #ifndef GIL_GIL_H
 #define GIL_GIL_H
 
+#include <cstdint>
+
+typedef std::uint32_t DWORD;///<@brief 4-х байтовый беззнаковый целочисленный тип (двойное слово)
+typedef std::uint16_t WORD;///<@brief 2-х байтовый беззнаковый целочисленный тип (слово)
+typedef std::uint8_t  BYTE;///<@brief Байтовый беззнаковый целочисленный тип (байт)
+
+/**
+ * @brief Структура, описывающая заголовок BMP файла.
+ */
+typedef struct {
+    WORD  bfType;///<@memberof ^FileHeader @brief Тип BMP, возможные значения: BM, BA, CI, CP, IC, PT. @warning В лаб.работах используется BM.
+    DWORD bfSize;///<@brief Размер файла в байтах.
+    WORD  bfReserved1;///<@brief служебное поле.
+    WORD  bfReserved2;///<@brief служебное поле.
+    DWORD bfOffBits;///<@brief Смещение от начала файла до таблицы с пикселами.
+} FileHeader;
+
+/**
+ * @brief Структура, описывающая информационный блок BMP файла.
+ */
+typedef struct {
+    DWORD biSize;///<@brief Размер информационного блока в байтах.
+    DWORD biWidth;///<@brief Ширина изображения в пикселах.
+    DWORD biHeight;///<@brief Высота изображения в пикселах.
+    WORD  biPlanes;///<@brief служебное поле, всегда равно 1.
+    WORD  biBitCount;///<@brief Количество бит, кодирующих цвет пиксела.
+    DWORD biCompression;///<@brief Метод сжатия. @warning На данный момент поддерживается только несжатые файлы.
+    DWORD biSizeImage;///<@brief Размер таблицы с пикселами в байтах.
+    DWORD biXPelsPerMeter;///<@brief Горизонтальное разрешение изображения pixel/m (resolution).
+    DWORD biYPelsPerMeter;///<@brief Вертикальное разрешение изображения pixel/m (resolution).
+    DWORD biClrUsed;///<@brief Количество цветов в палитре.
+    DWORD biClrImportant;///<@brief Количество используемых цветов, 0 если используются все. @warning Как правило не используется.
+} InfoHeader;
+
+/***
+ * @brief Структура, описывающая цвет.
+ * @warning Каналы записаны в обратном порядке, т.е. ABGR,а не RGBA
+ */
+typedef struct {
+    BYTE rgbBlue;///<@brief Значение синего канала
+    BYTE rgbGreen;///<@brief Значение зеленого канала
+    BYTE rgbRed;///<@brief Значение красного канала
+    BYTE rgbReserved;///<@warning Походу всегда 0 в BMP grayscale 8bpp
+} RGB_32;
+
+/***
+ * @namespace BMP
+ * @brief Основное рабочее пространство имен библиотеки.
+ * @warning В приложениях не рекомендуется использовать классы и функции, объявленные вне этого пространнства имен.
+ */
+namespace BMP {
+    /***
+     * @class Bitmap
+     * @brief Класс для работы с файлами формата .bmp
+     * @warning На данный момент функциональность ограничена (например, читаются только несжатые файлы).
+     */
+    class Bitmap {
+    private:
+        FileHeader bmfh;///<@brief Заголовок файла.
+        InfoHeader bmih;///<@brief Информационны блок файла.
+        RGB_32     aColors[];///<@brief Таблица цветов, количество цветов можно узнать в bmih.biClrUsed. Каждый цвет представляет RGB_32
+        BYTE       aBitmapBits[];///<@brief Таблица пикселов. @warning Используется выравнивание до 4 байт, т.е. если ширина изображение 6 пикселов, то размер строки будет не 6 байт, а 8 байт
+    public:
+        explicit Bitmap(const char* fileName);///<@brief Конструктор, загружающий изображение из файла. @param [in] fileName Строка, описывающая путь к файлу.
+        /***
+         * @brief Конструктор, создающий пустое изображение указанного размера.
+         * @param [in] width Ширина изображения в пикселах
+         * @param [in] height Высота изображения в пикселах
+         * @param [in] bitCount Глубина цвета бит/пиксел. По умолчанию 8.
+         */
+        Bitmap(DWORD width, DWORD height, WORD bitCount = 8);
+        void Save(const char* filename) const;///<@brief Метод, сохраняющий изображение в файл. @param [in] fileName Строка, описывающая путь к файлу.
+        virtual ~Bitmap();///<Обычный деконструктор.
+    };
+}
 #endif //GIL_GIL_H
